e3bb541b77d2d6df5adc3e46e30e8b8c6e43346e
Jozef Matus <jojo@matus.sk>
2018-04-25 17:35:57 +0200

    init

---
+++ TestApp/app/src/main/java/io/blueberry/testapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.blueberry.testapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //apikey: "83cc3555a53ac3319c30a33d38f6e041"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
